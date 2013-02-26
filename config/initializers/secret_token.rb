927fe18b30335b81cb74b3fc4eb5484753ab266b
Sjur Seibt <sjurlur@gmail.com>
2013-02-26 11:51:38 +0100

    HippHurra

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plantagotchi::Application.config.secret_token = '0f7464a83285a05fd2e84b92af63a60821a3c1b8372f2eb806a5a661e5e61051ae0ad6e0cdc80a0171f5bb5b5a9c390bbdf3cd536043a35720f0a178daefdffb'
